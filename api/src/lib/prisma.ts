b70a22fdb377f7fd0c78a8e43f51cac06799d554
Jason Rauch <Jeah84@users.noreply.github.com>
2026-04-12 02:55:23 -0700

    fix: add prisma generate to build step

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,4 +1,2 @@
-// @ts-ignore
-import pkg from '@prisma/client';
-// @ts-ignore
-export const prisma = new pkg.PrismaClient();
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
