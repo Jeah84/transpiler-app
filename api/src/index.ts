51918ccb6a631d67c7d267ff877e0c2c73a96968
Jason Rauch <Jeah84@users.noreply.github.com>
2026-04-13 01:53:23 -0700

    debug: expanded env check with all vars

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,12 +3,16 @@ import { env } from './config/env';
 
 app.listen(env.port, () => {
   console.log(`Server running on port ${env.port}`);
-  console.log('ENV CHECK:', {
+  console.log('ENV CHECK v2:', {
     hasJwtSecret: !!env.jwtSecret,
     jwtSecretLength: env.jwtSecret?.length,
     hasEmailVerifySecret: !!env.emailVerifySecret,
     emailVerifySecretLength: env.emailVerifySecret?.length,
+    hasStripeKey: !!env.stripeSecretKey,
+    stripeKeyLength: env.stripeSecretKey?.length,
     hasDatabaseUrl: !!env.databaseUrl,
     hasFrontendUrl: !!env.frontendUrl,
+    hasBrevoKey: !!env.brevoApiKey,
+    hasTogetherKey: !!env.togetherApiKey,
   });
 });
