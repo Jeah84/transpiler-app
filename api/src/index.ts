0c6ca53a9ec5095524efed362c2775390c31ac7e
Jason Rauch <Jeah84@users.noreply.github.com>
2026-04-13 00:44:33 -0700

    debug: log env var presence at startup

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,4 +3,12 @@ import { env } from './config/env';
 
 app.listen(env.port, () => {
   console.log(`Server running on port ${env.port}`);
+  console.log('ENV CHECK:', {
+    hasJwtSecret: !!env.jwtSecret,
+    jwtSecretLength: env.jwtSecret?.length,
+    hasEmailVerifySecret: !!env.emailVerifySecret,
+    emailVerifySecretLength: env.emailVerifySecret?.length,
+    hasDatabaseUrl: !!env.databaseUrl,
+    hasFrontendUrl: !!env.frontendUrl,
+  });
 });
