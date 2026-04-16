a69701fadfc3f1120506e7bf69f8c8232b8e9a79
Jason Rauch <Jeah84@users.noreply.github.com>
2026-04-16 01:40:55 -0700

    feat: activate Vercel Speed Insights in main.tsx and install package

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,12 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+// Vercel Speed Insights
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
